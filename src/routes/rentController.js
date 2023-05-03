977029e60e0dab237cbdfe00ae60401f5e6cb87e
wilyCesde <wruizza@cesde.net>
2023-05-03 07:54:06 -0500

    ver listas

--- src/routes/rentController.js
+++ src/routes/rentController.js
@@ -9,7 +9,6 @@ router.get('/add', (req, res) => {
   res.render('addRent');
 });
 
-// Acción para guardar un alquiler
 router.post('/add', async (req, res) => {
   const { rentnumber, username, platenumber, rentdate } = req.body;
 
@@ -18,16 +17,40 @@ router.post('/add', async (req, res) => {
     const car = await Car.findOne({ platenumber });
 
     if (!user || !car) {
-      return res.status(400).send('El usuario o el automóvil no existen');
+      return res.status(400).send('ya existe');
     }
 
-    const rent = new Rent({ rentnumber, username, platenumber, rentdate });
-    await rent.save();
+    const rent = await Rent.findOne({ rentnumber });
+    if (rent) {
+      return res.status(400).send('El número de alquiler ya existe');
+    }
+
+    if (car.state !== 'disponible') {
+      return res.status(400).send('El carro no está disponible');
+    }
+
+    const newRent = new Rent({ rentnumber, username, platenumber, rentdate });
+    await newRent.save();
+
+    // Cambiar estado del carro a "alquilado"
+    car.state = 'alquilado';
+    await car.save();
+
     res.redirect('/rents/list');
   } catch (err) {
     console.log(err);
     res.redirect('/rents/add');
   }
+});
+
+// Acción para listar alquileres
+router.get('/list', async (req, res) => {
+  const rents = await Rent.find();
+  res.render('listRents', { rents });
+});
+
+router.get('*', (req, res) => {
+  res.redirect('/rents/list');
 });
 
 // Acción para listar alquileres
