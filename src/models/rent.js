833feb72b7be9508bf4cb7cebd5da523005d4713
wilyCesde <wruizza@cesde.net>
2023-05-03 06:54:06 -0500

    funcional

--- src/models/rent.js
+++ src/models/rent.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  password: { type: String, required: true },
+const rentSchema = new mongoose.Schema({
+  rentnumber: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
+  platenumber: { type: String, required: true },
+  rentdate: { type: Date, required: true },
 });
 
-module.exports = mongoose.model('rent', UserSchema);
+module.exports = mongoose.model('rent', rentSchema);
